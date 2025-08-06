eff6da7998cbf0daf4caa08d42a8afcc0031dd26
victor-Manoel-borges <vb230216@gmail.com>
2025-08-06 16:45:23 -0300

    Add files via upload

---
+++ atividade 1 codigo  multiplicação.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    
+    int x;
+    float y; 
+    float s;
+    
+    x=30;
+    y=2.2;
+    s=x*y;
+
+    printf("A multiplicação dos valores x e y eh: %f", s);
+}
